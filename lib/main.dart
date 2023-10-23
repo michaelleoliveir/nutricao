b349e5b4eb4c63060be2e724aa06186b63926a20
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2023-10-22 21:23:20 -0300

    página 01 - (quase )OK
    falta dar uma ajustada nos elementos, pq o backgroundImage, o botão e os ícones não estão fixos a tela... sempre que o teclado abre para inserir texto no TextField do email e senha, esses elementos que eu citei se "elevam" junto com o teclado, causando erro

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nutrição',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFCEDEBD)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -35,16 +36,20 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
+          DecoratedBox(
+            position: DecorationPosition.background,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage('assets/background.png'),
-                  fit: BoxFit.cover,
-                  opacity: 0.30),
+                image: AssetImage('assets/background.png'),
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+                opacity: 0.20,
+              ),
             ),
+            child: Container(),
           ),
           Positioned(
-            top: 225, // Ajuste a posição vertical
+            top: 120, // Ajuste a posição vertical
             left: 122, // Ajuste a posição horizontal
             child: Image.asset(
               'assets/user.png',
@@ -56,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               children: <Widget>[
                 const SizedBox(
-                  height: 375,
+                  height: 270,
                 ),
                 Container(
                   margin: const EdgeInsets.all(20),
@@ -65,10 +70,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     decoration: InputDecoration(
                         hintText: "Email",
                         hintStyle: const TextStyle(
-                            fontSize: 18,
-                            fontStyle: FontStyle.normal,
-                            color: Color(0xFF435334),
-                            fontFamily: 'Inter'),
+                          fontSize: 18,
+                          fontStyle: FontStyle.normal,
+                          color: Color(0xFF435334),
+                        ),
                         contentPadding: const EdgeInsets.all(13),
                         filled: true,
                         fillColor: const Color(0xFF9EB384),
@@ -85,10 +90,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     decoration: InputDecoration(
                         hintText: "Senha",
                         hintStyle: const TextStyle(
-                            fontSize: 18,
-                            fontStyle: FontStyle.normal,
-                            color: Color(0xFF435334),
-                            fontFamily: 'Inter'),
+                          fontSize: 18,
+                          fontStyle: FontStyle.normal,
+                          color: Color(0xFF435334),
+                        ),
                         contentPadding: const EdgeInsets.all(13),
                         filled: true,
                         fillColor: const Color(0xFF9EB384),
@@ -98,25 +103,80 @@ class _MyHomePageState extends State<MyHomePage> {
                                 const BorderSide(color: Colors.transparent))),
                   ),
                 ),
-                const Stack(
-                  children: [
-                    Positioned(
-                      right: 30,
-                      left: 30,
-
-                      child: Text(
-                        'Esqueci minha senha',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 102, 102, 102),
-                          fontFamily: 'Inter',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
               ],
             ),
-          )
+          ),
+          const Positioned(
+            left: 232,
+            child: Text(
+              'Esqueci minha senha',
+              style: TextStyle(height: 65, color: Color(0xFF435334)),
+            ),
+          ),
+          const Positioned(
+              bottom: 229,
+              left: 135,
+              child: ElevatedButton(
+                onPressed: null,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Color(0xFF9EB384)),
+                  minimumSize: MaterialStatePropertyAll(Size(130, 50)),
+                ),
+                child: Text(
+                  'Entrar',
+                  style: TextStyle(fontSize: 18, color: Color(0xFF435334)),
+                ),
+              )),
+          Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 610),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: null,
+                      icon: Icon(Icons.facebook),
+                      style: ButtonStyle(
+                          iconSize: MaterialStatePropertyAll(40),
+                          iconColor:
+                              MaterialStatePropertyAll(Color(0xFF435334)),
+                          alignment: Alignment.center),
+                    ),
+                    IconButton(
+                      onPressed: null,
+                      icon: Icon(Icons.email),
+                      style: ButtonStyle(
+                          iconSize: MaterialStatePropertyAll(40),
+                          iconColor:
+                              MaterialStatePropertyAll(Color(0xFF435334)),
+                          alignment: Alignment.center),
+                    ),
+                    IconButton(
+                      onPressed: null,
+                      icon: FaIcon(FontAwesomeIcons.google),
+                      style: ButtonStyle(
+                          iconSize: MaterialStatePropertyAll(30),
+                          iconColor:
+                              MaterialStatePropertyAll(Color(0xFF435334)),
+                          alignment: Alignment.center),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: 250,
+                height: 1,
+                color: const Color(0xFF818877),
+              ),
+              const Text(
+                'Cadastre-se',
+                style: TextStyle(
+                    height: 3, color: Color(0xFF818877), fontSize: 15),
+              )
+            ],
+          ),
         ],
       ),
     );
