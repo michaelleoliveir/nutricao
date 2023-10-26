cdb43378b73f2a2f3c3fb6594f630dd18f6fb902
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2023-10-25 21:33:52 -0300

    início da main page
    obs: está dando um erro no route da login page com a main que ainda não descobri como arrumar

---
+++ lib/screens/login.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/background.png'),
+                  fit: BoxFit.cover,
+                  colorFilter:
+                      ColorFilter.mode(Colors.white, BlendMode.dstATop),
+                  opacity: 0.30)),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            children: [
+              Positioned(
+                top: 120, // Ajuste a posição vertical
+                left: 122, // Ajuste a posição horizontal
+                child: Image.asset(
+                  'assets/user.png',
+                  width: 147, // Ajuste a largura
+                  height: 147, // Ajuste a altura
+                ),
+              ),
+              Center(
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 270,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(20),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            hintText: "Email",
+                            hintStyle: const TextStyle(
+                              fontSize: 18,
+                              fontStyle: FontStyle.normal,
+                              color: Color(0xFF435334),
+                            ),
+                            contentPadding: const EdgeInsets.all(13),
+                            filled: true,
+                            fillColor: const Color(0xFF9EB384),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: const BorderSide(
+                                    color: Colors.transparent))),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(20),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            hintText: "Senha",
+                            hintStyle: const TextStyle(
+                              fontSize: 18,
+                              fontStyle: FontStyle.normal,
+                              color: Color(0xFF435334),
+                            ),
+                            contentPadding: const EdgeInsets.all(13),
+                            filled: true,
+                            fillColor: const Color(0xFF9EB384),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: const BorderSide(
+                                    color: Colors.transparent))),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Positioned(
+                left: 232,
+                child: Text(
+                  'Esqueci minha senha',
+                  style: TextStyle(height: 65, color: Color(0xFF435334)),
+                ),
+              ),
+              const SingleChildScrollView(
+                child: Center(
+                  heightFactor: 21,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/main');
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Color(0xFF9EB384)),
+                      minimumSize: MaterialStatePropertyAll(Size(130, 50)),
+                    ),
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(fontSize: 18, color: Color(0xFF435334)),
+                    ),
+                  ),
+                ),
+              ),
+              const SingleChildScrollView(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 600),
+                      child: IconButton(
+                        onPressed: null,
+                        icon: Icon(Icons.facebook),
+                        style: ButtonStyle(
+                            iconSize: MaterialStatePropertyAll(40),
+                            iconColor:
+                                MaterialStatePropertyAll(Color(0xFF435334)),
+                            alignment: Alignment.center),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 600),
+                      child: IconButton(
+                        onPressed: null,
+                        icon: Icon(Icons.email),
+                        style: ButtonStyle(
+                            iconSize: MaterialStatePropertyAll(40),
+                            iconColor:
+                                MaterialStatePropertyAll(Color(0xFF435334)),
+                            alignment: Alignment.center),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 600),
+                      child: IconButton(
+                        onPressed: null,
+                        icon: FaIcon(FontAwesomeIcons.google),
+                        style: ButtonStyle(
+                            iconSize: MaterialStatePropertyAll(30),
+                            iconColor:
+                                MaterialStatePropertyAll(Color(0xFF435334)),
+                            alignment: Alignment.center),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Cadastre-se',
+                        style: TextStyle(
+                          height: 97,
+                          color: Color.fromARGB(255, 67, 72, 60),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
